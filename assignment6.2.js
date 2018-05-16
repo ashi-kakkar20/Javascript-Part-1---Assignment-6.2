80d095c921469b78e8cb5a4f10aafb667a360379
ashi-kakkar20 <38372085+ashi-kakkar20@users.noreply.github.com>
2018-05-16 21:42:46 +0530

    Add files via upload

---
+++ assignment6.2.js
@@ -0,0 +1,23 @@
+// This assignment helps to master skills on JavaScript variables and data types.
+var recipeName ="Creamy squash linguine";
+var preparationTime=45;
+var recipeBy ="Jimmy Cutter";
+var isVegetarian = true;
+var calories = 223.7;
+
+console.log("Recipe Details");
+console.log("----------------");
+console.log("Recipe Name:"+ recipeName);
+console.log("Preparation Time:"+ preparationTime);
+console.log("Recipe By:" + recipeBy);
+console.log("is Vegetarian Dish:" +isVegetarian);
+console.log("Calories:" +calories);
+console.log("Recipe Details after Update");
+console.log("----------------------------");
+preparationTime=60;
+recipeBy="Jimmy Cutter George";
+console.log("Recipe Name:"+ recipeName);
+console.log("Preparation Time:"+ preparationTime);
+console.log("Recipe By:" + recipeBy);
+console.log("is Vegetarian Dish:" +isVegetarian);
+console.log("Calories:" +calories);
